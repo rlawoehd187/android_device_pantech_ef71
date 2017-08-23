20edab218c8f34de3b795aa704d56873d489a17e
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-23 08:35:15 -0700

    ef71: Add mediaserver seccomp policies
    Change-Id: I70ab0cd61e8bf8bceb9553534157e03178ea89af

--- device.mk
+++ device.mk
@@ -269,6 +269,11 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     librmnetctl
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
