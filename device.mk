0c3d2dea7ff99df13ac48d30b14dfcc94c650d64
LuK1337 <priv.luk@gmail.com>
2018-04-14 18:11:47 +0300

    ef71: Convert audio_effects.conf to XML
     * Converted using : https://github.com/luk1337/aeffects-conf2xml

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
 
 #Audio mixer
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/vendor/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
