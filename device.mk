524dfd0d416df72ae41188302493d7b2865067c8
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    ef71: Adding Audio HAL V5: Direction API
    Bug: 119137468
    Test: With testbed app
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- device.mk
+++ device.mk
@@ -80,25 +80,19 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0 \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.common@4.0 \
-    android.hardware.audio.common@4.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
-    audio.primary.msm8937 \
+    audio.primary.msm8953 \
     audio.r_submix.default \
     audio.usb.default \
+    libaacwrapper \
     libaudio-resampler \
-    libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
+    libqcompostprocbundle \
     tinymix
 
 # Audio mixer
