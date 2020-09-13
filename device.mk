bc2ce4c9ad0ef588540632cd7b1688dd6302b39d
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:18:28 +0800

    ef71: Add prebuilt protobuf from sdk29
    This is needed for our prebuilt DRM library.
    
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- device.mk
+++ device.mk
@@ -437,6 +437,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v28/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v28.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
