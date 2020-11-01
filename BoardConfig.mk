b63e79a37a2ec5ac1cc8ce0e6f2d210e38c85fb1
Bruno Martins <bgcngm@gmail.com>
2020-11-01 14:45:24 +0000

    ef71: Replace BOARD_CHARGER_DISABLE_INIT_BLANK with equivalent prop
    Change-Id: I00125a429cde213e8dd22f742c5875e39b80bb5e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,9 +127,6 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
     /system/bin/mediaserver=22 \
     /system/vendor/bin/mm-qcamera-daemon=22
 
-# Charger
-BOARD_CHARGER_DISABLE_INIT_BLANK := true
-
 # Clang
 TARGET_USE_SDCLANG := true
 
