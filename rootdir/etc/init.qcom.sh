3b33cb02bd80862dd1c0379ded60cd323849764b
rlawoehd187 <rlawoehd187@naver.com>
2020-11-27 22:07:49 +0900

    ef71: rootdir: Use /data/vendor/radio for radio data folder
    Use /data/vendor partition to hold vendor radio data. Modify radio
    related init rc and scripts.

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -30,22 +30,22 @@
 # Make modem config folder and copy firmware config to that folder for RIL
 #
 if [ -f /data/misc/radio/ver_info.txt ]; then
-    prev_version_info=`cat /data/misc/radio/ver_info.txt`
+    prev_version_info=`cat /data/vendor/radio/ver_info.txt`
 else
     prev_version_info=""
 fi
 
 cur_version_info=`cat /firmware/verinfo/ver_info.txt`
 if [ "$prev_version_info" != "$cur_version_info" ]; then
-    rm -rf /data/misc/radio/modem_config
-    mkdir /data/misc/radio/modem_config
-    chmod 770 /data/misc/radio/modem_config
-    cp -r /firmware/image/modem_pr/mcfg/configs/* /data/misc/radio/modem_config
-    chown -hR radio.radio /data/misc/radio/modem_config
-    cp /firmware/verinfo/ver_info.txt /data/misc/radio/ver_info.txt
-    chown radio.radio /data/misc/radio/ver_info.txt
+    rm -rf /data/vendor/radio/modem_config
+    mkdir /data/vendor/radio/modem_config
+    chmod 770 /data/vendor/radio/modem_config
+    cp -r /firmware/image/modem_pr/mcfg/configs/* /data/vendor/radio/modem_config
+    chown -hR radio.radio /data/vendor/radio/modem_config
+    cp /firmware/verinfo/ver_info.txt /data/vendor/radio/ver_info.txt
+    chown radio.radio /data/vendor/radio/ver_info.txt
 fi
-echo 1 > /data/misc/radio/copy_complete
+echo 1 > /data/vendor/radio/copy_complete
 
 # Set Memory paremeters.
 #
