7e3474d931da8ff6ffd5d94c8d2f504b66266260
rlawoehd187 <rlawoehd187@naver.com>
2020-11-27 21:59:55 +0900

    임시2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,5 +225,6 @@ WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 
+PRODUCT_VENDOR_MOVE_ENABLED := true
 # Inherit the proprietary files
 -include vendor/pantech/ef71/BoardConfigVendor.mk
