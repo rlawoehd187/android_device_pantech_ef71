e3263b8f1aaea72ba5aa90719686d949699e5fb7
Jacob Deepu <d2jdeepu@gmail.com>
2020-12-05 22:17:08 +0530

    ef71: Load prebuilt protobuf from v29
    * Patch blobs to load the lib
    
    Change-Id: I8ec1fe4f7f8931e762af958df451695db4ec89ac

--- device.mk
+++ device.mk
@@ -427,6 +427,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite-v29.so
+
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
