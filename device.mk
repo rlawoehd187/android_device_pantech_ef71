730ed7be336212cee58334b240590ef3ac55940a
hiru <rlawoehd187@naver.com>
2020-12-25 20:49:23 +0900

    ef71: Update ril for Pie.

--- device.mk
+++ device.mk
@@ -323,8 +323,13 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0 \
     libcnefeatureconfig \
-    librmnetctl
+    librmnetctl \
+    libprotobuf-cpp-full \
+    libxml2
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
