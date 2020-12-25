9199d2223ea2e5390820437464cec1891d0e305e
hiru <rlawoehd187@naver.com>
2020-12-25 20:02:27 +0900

    ef71: build libandroid_net

--- device.mk
+++ device.mk
@@ -275,7 +275,8 @@ PRODUCT_COPY_FILES += \
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
-    netutils-wrapper-1.0
+    netutils-wrapper-1.0 \
+    libandroid_net
 
 # IPv6
 PRODUCT_PACKAGES += \
