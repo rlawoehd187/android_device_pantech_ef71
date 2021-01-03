995e53c8b2879e84020231bb7910be54363b87c9
hiru <rlawoehd187@naver.com>
2021-01-04 00:16:12 +0900

    임시커밋

--- device.mk
+++ device.mk
@@ -129,12 +129,19 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    GCam
+
+PRODUCT_PACKAGES += \
+    libshim_cameraclient \
+    libshim_mutexdestroy \
     libshim_camera \
-    libshim_cameraclient
+    libshim_pthreadts
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/imx214_chromatix.xml:system/etc/camera/imx214_chromatix.xml \
@@ -236,11 +243,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
-
-# Libshims
-PRODUCT_PACKAGES += \
-    libshim_mutexdestroy \
-    libshim_pthreadts
 
 # Lights
 PRODUCT_PACKAGES += \
