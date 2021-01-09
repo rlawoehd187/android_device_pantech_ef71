7fe9b53779172c65846810dc2d9dc1889b2eb6a0
hiru <rlawoehd187@naver.com>
2021-01-10 01:24:34 +0900

    ef71: Remove android.hidl.manager@1.0
    No blobs depend on it now.

--- device.mk
+++ device.mk
@@ -209,7 +209,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
 PRODUCT_COPY_FILES += \
