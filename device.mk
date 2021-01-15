22f71321294c09de86f7ab172a8d962f4924f65a
hiru <rlawoehd187@naver.com>
2021-01-16 04:35:55 +0900

    ef71: Add some configuration for GPS.

--- device.mk
+++ device.mk
@@ -378,6 +378,7 @@ PRODUCT_PACKAGES += \
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-com.qualcomm.location.xml:system/etc/permissions/privapp-permissions-com.qualcomm.location.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
 # WiFi HAL
