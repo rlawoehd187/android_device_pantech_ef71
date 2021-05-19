c218414f8b8832a6aaa8867b4c5495549ac6a91c
hiru <rlawoehd187@naver.com>
2021-05-19 21:09:16 +0900

    ef71: Drop Gcam & Set default to Snap

--- device.mk
+++ device.mk
@@ -141,7 +141,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    GCam
+    Snap
 
 PRODUCT_PACKAGES += \
     libshim_cameraclient \
