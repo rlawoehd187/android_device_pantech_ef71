3f95b84e390f79b09a18cbdf29f29e147407e900
hiru <rlawoehd187@naver.com>
2021-11-03 03:47:15 +0900

    ef71: Define a soong namespace
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- device.mk
+++ device.mk
@@ -382,6 +382,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
