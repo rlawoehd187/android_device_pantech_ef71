496934f9c552cd51cd91dbb9db7bb4cc3785865b
hiru <rlawoehd187@naver.com>
2021-11-08 06:14:45 +0900

    ef71: Drop snap
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- device.mk
+++ device.mk
@@ -143,8 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-ef71 \
     cameraserver-ef71 \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    snap
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 PRODUCT_PACKAGES += \
     libshim_cameraclient \
