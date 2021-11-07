788e279a2b3fb42387a16e233929e09dc9db735f
hiru <rlawoehd187@naver.com>
2021-11-08 04:13:53 +0900

    ef71: Import customized camera hals. * We can't use Hal3 anymore. We need a new library.
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- device.mk
+++ device.mk
@@ -139,12 +139,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl-ef71 \
+    android.hardware.camera.provider@2.4-impl-ef71 \
+    cameraserver-ef71 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
-    Snap
+    snap
 
 PRODUCT_PACKAGES += \
     libshim_cameraclient \
