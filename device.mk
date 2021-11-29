deb9196b9988bd130f1695a1d2fd556ccd5a0275
hiru <rlawoehd187@naver.com>
2021-11-29 18:03:43 +0900

    Revert "ef71: Import customized camera hals."
    This reverts commit 788e279a2b3fb42387a16e233929e09dc9db735f.
    
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- device.mk
+++ device.mk
@@ -140,9 +140,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl-ef71 \
-    android.hardware.camera.provider@2.4-impl-ef71 \
-    cameraserver-ef71 \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
