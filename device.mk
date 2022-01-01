f87c65f5cc369ef747ac8ea09c5d80544f768b48
hiru <rlawoehd187@naver.com>
2022-01-01 19:41:20 +0900

    ef71: Forced to legacy camera for now. * The camera library we have crashes when operating with HAL3 in Q or higher.
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- device.mk
+++ device.mk
@@ -138,11 +138,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    Snap
 
 PRODUCT_PACKAGES += \
     libshim_cameraclient \
