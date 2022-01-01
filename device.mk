65803b4e70cc622fa8c1b10f407e9455f3fde8f3
hiru <rlawoehd187@naver.com>
2022-01-01 21:14:31 +0900

    Revert "ef71: Forced to legacy camera for now."
    This reverts commit f87c65f5cc369ef747ac8ea09c5d80544f768b48.

--- device.mk
+++ device.mk
@@ -138,11 +138,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
-    camera.device@1.0-impl-legacy \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    Snap
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 PRODUCT_PACKAGES += \
     libshim_cameraclient \
