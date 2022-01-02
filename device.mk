5c976fe943038cc9d501ec14fae87b6a43cbebef
hiru <rlawoehd187@naver.com>
2022-01-03 05:41:42 +0900

    ef71: enable livedisplay.
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- device.mk
+++ device.mk
@@ -271,6 +271,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8937
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-service \
