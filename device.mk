670021b10332605e52af28a71759daf7e4359b96
hiru <rlawoehd187@naver.com>
2022-01-15 03:45:21 +0900

    ef71: Drop VNDK-SP
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- device.mk
+++ device.mk
@@ -426,10 +426,6 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
-# VNDK
-PRODUCT_PACKAGES += \
-    vndk-sp
-
 # Whitelisted app
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
